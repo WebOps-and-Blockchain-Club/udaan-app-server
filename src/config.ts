0051044fccad587a451500bf8dd39884a1d219a0
Purushottam <131853935+ProElecttro@users.noreply.github.com>
2023-12-18 17:22:26 +0530

    specified the loopback IP address.

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config()
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 5432,
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
